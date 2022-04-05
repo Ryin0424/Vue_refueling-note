ca49998e34b24fd53c9ff3a73ff454d5f15bafbf
阿陰 <a30729522@gmail.com>
2022-04-06 03:36:27 +0800

    [stash] user router & firebase

---
+++ src/views/Search.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="create-new">
+
+    <div class="circle-btn pull-right" @click="close"> <font-awesome-icon icon="fa-solid fa-xmark" />  </div>
+
+    <searchForm
+                @close="addSuccess" />
+
+  </div>
+
+</template>
+
+<script>
+import searchForm from '@/components/inputForm.vue';
+
+export default {
+  name: 'add-record',
+  components: {
+    searchForm
+  },
+  data:() => ({
+    searchVal: {
+      start: '',
+      end: '',
+    }
+  }),
+  methods:{
+    close(){
+      this.$emit('closeCreate');
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$dark-main-color: #091955;
+
+.create-new{
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 100vh;
+  width: 100vw;
+  background-color: $dark-main-color;
+}
+.pull-right{
+  position: absolute;
+  top: 40px;
+  right: 40px;
+}
+</style>
