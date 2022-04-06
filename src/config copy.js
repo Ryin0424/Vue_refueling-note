73dd002c4b0985ca60a645792b146eb50b84bff5
阿陰 <a30729522@gmail.com>
2022-04-06 14:07:15 +0800

    [fix] update ignore & add example config

---
+++ src/config copy.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "Your apiKey",
+  authDomain: "Your authDomain",
+  databaseURL: "Your databaseURL",
+  projectId: "Your projectId",
+  storageBucket: "Your storageBucket",
+  messagingSenderId: "Your messagingSenderId",
+  appId: "Your appId",
+  measurementId: "Your measurementId"
+}
+
+export default firebaseConfig;
